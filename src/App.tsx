7748d625febe3f1443edc4803fe25ae42f7f0900
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-17 22:29:07 +0100

    style(responsive): responsive
    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Filters from './components/Filters'
 import Footer from './components/Footer'
 import Form from './components/Form'
 import Header from './components/Header'
@@ -10,9 +9,8 @@ function App() {
   <div className='w-screen min-h-screen relative flex flex-col items-center overflow-hidden text-white'>
       <div className="absolute top-0  z-[-2] h-full w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <Header/>
-      <main className='flex flex-grow content-center w-full min-h-fit flex-col max-w-[1500px]'>
+      <main className='flex flex-grow content-center w-full min-h-fit flex-col max-w-md md:max-w-[700px] lg:max-w-[950px] xl:max-w-[1400px]'>
         <Form/>
-        <Filters />
         <MoviesContainer />
       </main>
       <Footer/>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Search from './icons/Search'
 import { useMovies } from '../hooks/useMovies'
+import { useMoviesContext } from '../hooks/useMoviesContext'
 
 function Form() {
+  const {sortMovies} = useMoviesContext()
   const {getMovies} = useMovies()
   const [value, setValue] = useState<string>('')
 
@@ -13,8 +15,8 @@ function Form() {
   }
 
   return (
-    <div className="relative w-full max-w-md mx-auto my-4">
-      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+    <div className="relative w-full max-w-sm md:max-w-md mx-auto my-4">
+      <div className="absolute top-5 start-0 flex items-center ps-3 pointer-events-none">
         <Search className='size-4'/>
       </div>
       <input 
@@ -25,6 +27,10 @@ function Form() {
         className="block w-full p-4 ps-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
         placeholder="Search Mockups, Logos..." 
         required />
+      <div className='mt-2'>
+        <label htmlFor="hs-default-checkbox" className="text-lg text-gray-500 mr-3 dark:text-neutral-400">Order (A-Z)</label>
+        <input type="checkbox"  onChange={sortMovies} className="shrink-0  border-gray-200 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-default-checkbox"/>
+      </div>
     </div>
   )
 }
