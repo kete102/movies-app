6a706fb3b335dc53b93dc9069a4631f84d5b4882
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-17 15:32:57 +0100

    feat(icons): github, search icons
    github, search icons

---
+++ src/components/icons/Search.tsx
@@ -0,0 +1,9 @@
+import type { SVGProps } from "react";
+const Search = (props: SVGProps<SVGSVGElement>) => (
+  <svg {...props}  className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+  </svg>
+
+)
+
+export default Search
