8b521afc767eea14527fce5c893b5669c3f48dcb
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-17 16:49:20 +0100

    style(app.tsx): app.tsx
    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <Header/>
       <main className='container flex-1 w-full max-w-[1300px] flex flex-col items-center'>
-        <section className='mt-6 w-full'>
+        <section className='mt-6 w-full flex items-center justify-center'>
           <Form/>
         </section>
         <section className='bg-white/5 rounded-md mt-4 flex-1 w-full'>
