3f74ab6d20e0fef7ce635a9fb2b6f532c334d0e8
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-17 17:56:42 +0100

    feat(moviescontainer): moviesContainer
    MoviesContainer component

--- src/components/MoviesContainer.tsx
+++ src/components/MoviesContainer.tsx
@@ -1,7 +1,30 @@
+import { useMoviesContext } from "../hooks/useMoviesContext"
+import Filters from "./Filters"
 
 function MoviesContainer() {
+  const {movies} = useMoviesContext()
+  console.log('Movies container: ', movies)
+
+  if(!movies) {
+    return (
+    <div className="w-full h-full flex items-center justify-center ">
+        <h3>Search  movies</h3>
+      </div>
+    )
+  }
   return (
-    <div>MoviesContainer</div>
+    <div className="w-full h-full flex flex-col p-2 overflow-y-scroll">
+      <section className="w-full flex justify-end px-2">
+        <Filters />
+      </section>
+      <section className="flex-1">
+        {movies.map((movie) => (
+        <article key={movie.id}>
+            <h1>{movie.title}</h1>
+          </article>
+        ))}
+      </section>
+    </div>
   )
 }
 
