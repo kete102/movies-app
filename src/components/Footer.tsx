349af0695b11c8b84ee2151fa57e5bf62e988b28
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-17 22:14:29 +0100

    style(header, footer): some styles adjustments
    some styles adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,7 @@ import Github from './icons/Github'
 
 function Footer() {
   return (
-
-      <footer className='flex items-center gap-x-2 p-2'>
+      <footer className='flex items-center justify-center mb-4 mt-8 gap-x-2 p-2'>
         <a href="https://github.com/kete102">
           Made with 🧡 by{' '}
           <strong className="inline-flex items-center gap-1 underline">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 function Header() {
   return (
-    <header className="space-y-3 pt-8 w-full text-center">
+    <header className="space-y-3 pt-8 text-center">
       <h1 className="font-bold  text-5xl tracking-wide">Movies app</h1>
       <p className="font-medium text-zinc-500 text-lg tracking-tight">Search your favorite movies</p>
     </header>
